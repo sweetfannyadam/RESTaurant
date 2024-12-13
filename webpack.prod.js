4cc7af4ceab37e18fc586e405c4e92c1c14bb8ce
elizabeth <elizabethclarap@gmail.com>
2024-12-13 17:01:47 +0700

    deploy to github pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "production",
-  // devtool: "source-map",
+  devtool: "source-map",
   module: {
     rules: [
       {
